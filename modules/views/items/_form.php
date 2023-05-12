d10ee22a4638bae05a0878aceb4a50d111afc5c3
robertaydinyan <66666666>
2023-05-12 22:34:05 +0400

    update items redesign

--- modules/views/items/_form.php
+++ modules/views/items/_form.php
@@ -14,44 +14,94 @@ $step = $model->getStep();
 ?>
 
 <div class="items-form">
-
     <?php $form = ActiveForm::begin(); ?>
+    <div class="row">
+        <div class="col-2">
+            <?= $form->field($model, 'i_type')->dropDownList(Items::getITypes(), ['multiple' => 'multiple', 'class' => 'required form-control item-type select2']); ?>
+            <?= $form->field($model, 'i_usg_type')->dropDownList(
+                \yii\helpers\ArrayHelper::map(\app\modules\models\UsgType::find()->asArray()->all(), 'id', 'name'),
+                ['multiple' => 'multiple', 'class' => 'required form-control item-usage-type select2', 'value' => $model->i_usg_type]
+            ); ?>
+            <?= $form->field($model, 'i_comb_type_id')->dropDownList(Items::getICombTypes(), ['multiple' => 'multiple', 'class' => 'item-comb-type select2']); ?>
 
-    <?= $form->field($model, 'title[1]')->textarea([
-        'maxlength' => true,
-        'value' => $model->getTitle(1)->title,
-        'class' => 'form-control required'
-    ])->label('title ru') ?>
-    <?= $form->field($model, 'description[1]')->textarea([
-        'maxlength' => true,
-        'value' => $model->getTitle(1)->description,
-        'class' => 'form-control required'
-    ])->label('description ru') ?>
+            <div class="row">
+                <?php echo sprintf('<div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                   <div class="color-spector color-spector-change color-for-%s">%s</div>
+                </div>',
 
-    <?= $form->field($model, 'title[2]')->textarea([
-        'maxlength' => true,
-        'value' => $model->getTitle(2)->title,
-        'class' => 'form-control required'
-    ])->label('title en') ?>
-    <?= $form->field($model, 'description[2]')->textarea([
-        'maxlength' => true,
-        'value' => $model->getTitle(2)->description,
-        'class' => 'form-control required'
-    ])->label('description en') ?>
 
-    <?= $form->field($model, 'i_type')->dropDownList(Items::getITypes(), ['multiple' => 'multiple', 'class' => 'required form-control item-type select2']); ?>
+                    $model->getColorSector(1)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(1)->color_id])->label(false),
+                    $model->getColorSector(2)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(2)->color_id])->label(false),
+                    $model->getColorSector(3)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(3)->color_id])->label(false),
+                    $model->getColorSector(4)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(4)->color_id])->label(false),
+                    $model->getColorSector(5)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(5)->color_id])->label(false),
+                    $model->getColorSector(6)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(6)->color_id])->label(false),
+                    $model->getColorSector(7)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(7)->color_id])->label(false),
+                    $model->getColorSector(8)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(8)->color_id])->label(false),
+                    $model->getColorSector(9)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(9)->color_id])->label(false),
+                    $model->getColorSector(10)->color_id,
+                    $form->field($model, 'colorSectors[color_id][]')->hiddenInput(['class' => 'required', 'value' => $model->getColorSector(10)->color_id])->label(false)
 
-    <?= $form->field($model, 'i_usg_type')->dropDownList(
-        \yii\helpers\ArrayHelper::map(\app\modules\models\UsgType::find()->asArray()->all(), 'id', 'name'),
-        ['multiple' => 'multiple', 'class' => 'required form-control item-usage-type select2', 'value' => $model->i_usg_type]
-    ); ?>
-    <?= $form->field($model, 'i_comb_type_id')->dropDownList(Items::getICombTypes(), ['multiple' => 'multiple', 'class' => 'item-comb-type select2']); ?>
+                );
+                ?>
+            </div>
+        </div>
+        <div class="col-10 row">
+            <?= $form->field($model, 'title[1]', ['options' => ['class' => 'col-6']])->textarea([
+                'maxlength' => true,
+                'value' => $model->getTitle(1)->title,
+                'class' => 'form-control required'
+            ])->label('title ru') ?>
+            <?= $form->field($model, 'description[1]', ['options' => ['class' => 'col-6']])->textarea([
+                'maxlength' => true,
+                'value' => $model->getTitle(1)->description,
+                'class' => 'form-control required'
+            ])->label('description ru') ?>
 
-    <?php for($i = 1; $i <= 10; $i++) {
-        echo $form->field($model, 'colorSectors[color_id][]')->dropDownList(ItemColors::getColorRange(),['value' => $model->getColorSector($i)->color_id])->label('Sector ' . $i);
-        echo $form->field($model, 'colorSectors[description_ru][' . ($i - 1) . ']')->textarea(['class' => 'form-control required result-description', 'value' => $model->getColorSector($i)->description_ru])->label('Description ru');
-        echo $form->field($model, 'colorSectors[description_en][' . ($i - 1) . ']')->textarea(['class' => 'form-control required result-description', 'value' => $model->getColorSector($i)->description_en])->label('Description en');
-    } ?>
+            <?= $form->field($model, 'title[2]', ['options' => ['class' => 'col-6']])->textarea([
+                'maxlength' => true,
+                'value' => $model->getTitle(2)->title,
+                'class' => 'form-control required'
+            ])->label('title en') ?>
+            <?= $form->field($model, 'description[2]', ['options' => ['class' => 'col-6']])->textarea([
+                'maxlength' => true,
+                'value' => $model->getTitle(2)->description,
+                'class' => 'form-control required'
+            ])->label('description en') ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <?php for($i = 0; $i < 10; $i++) {
+            echo sprintf('<div class="col-6 d-flex justify-content-between">
+                    <span>%s</span>
+                    %s
+                    %s 
+                </div>',
+                10 * $i + 1 . '-' . 10 * ($i + 1),
+                $form->field($model, 'colorSectors[description_ru][' . $i . ']', ['options' => ['class' => 'col-5']])->textarea(['class' => 'form-control required result-description', 'value' => $model->getColorSector($i + 1)->description_ru])->label('Description ru'),
+                $form->field($model, 'colorSectors[description_en][' . $i . ']', ['options' => ['class' => 'col-5']])->textarea(['class' => 'form-control required result-description', 'value' => $model->getColorSector($i + 1)->description_en])->label('Description en')
+            );
+        } ?>
+    </div>
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
     </div>
